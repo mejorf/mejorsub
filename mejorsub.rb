2fc6476b9042de8545d6267239979d9f7dc97d8c
mejorf <mejorfelippe@gmail.com>
2017-03-09 12:50:35 -0300

    add optparse

--- mejorsub.rb
+++ mejorsub.rb
@@ -1,20 +1,24 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
 require 'net/http'
 require 'digest'
 
 # Generates an object that downloads subtitles from subdb
 class SubDownloader
-  def initialize(ext, directory, language = 'pt')
-    @directory = directory
-    if File.directory?(@directory)
-      @ext = ext
-      @language = language
+  def initialize(options)
+    @target = ARGV[0]
+    @language = options[:language]
+    if File.directory?(@target)
+      @ext = options[:extension]
+      @ext = '*/*.{avi,mp4,mkv,mpeg,flv,rm,wmv,m4v}' if @ext.nil?
       @files = listfiles
       @files_and_hashes = gen_hashes
       downloadall
-    elsif File.file?(@directory)
-      download(@directory)
+    elsif File.file?(@target)
+      @files = [@target]
+      @files_and_hashes = gen_hashes
+      download @target
     else
       abort('Argument must be a file or a directory path')
     end
@@ -35,10 +39,10 @@ class SubDownloader
 
   # generate an array of files in a directory
   def listfiles
-    if @directory[-1] != '/'
-      Dir[@directory + '/*' + @ext]
+    if @target[-1] != '/'
+      Dir[@target + '/*' + @ext]
     else
-      Dir[@directory + '*' + @ext]
+      Dir[@target + '*' + @ext]
     end
   end
 
@@ -56,11 +60,11 @@ class SubDownloader
   # downloads from individual hashes
   def download(hash)
     header = { 'User-Agent' => 'SubDB/1.0 (MejorSub /1.0;
-        https://github.com/mejorf/mejorsub)' }
+      https://github.com/mejorf/mejorsub)' }
     http = Net::HTTP.new 'api.thesubdb.com'
     begin
       download = http.send_request('GET', '/?action=download&hash=' + hash +
-                                   "&language=#{@language}", nil, header)
+        "&language=#{@language}", nil, header)
       out = @files_and_hashes.key(hash).gsub(/\.[^.]*$/, '.srt')
       File.open(out, 'wb') { |f| f.write(download.body) }
       puts out + ' sucessfully downloaded'
@@ -70,11 +74,27 @@ class SubDownloader
   end
 
   def downloadall
-    puts @files_and_hashes
     @files_and_hashes.each do |_filename, hash|
       download hash
     end
   end
 end
 
-SubDownloader.new ARGV[0], ARGV[1], ARGV[2]
+options = {}
+opt_parser = OptionParser.new do |opt|
+  opt.banner = 'Usage: opt_parser [OPTIONS] file_or_dir_path'
+  opt.on('-l arg', '--language=arg', 'select target language to the
+         subtitle(s)') do |lang|
+    options[:language] = lang
+  end
+  opt.on('-e arg', '--extension=arg', 'manually select the video(s)
+          extension(s)') do |ext|
+    options[:extension] = ext
+  end
+  opt.on('-h', '--help', 'help') do
+    puts opt_parser
+  end
+end
+
+opt_parser.parse!
+SubDownloader.new(options)
