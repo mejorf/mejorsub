c79a902ebb1ce63e582e513bdce1c3cc6b135fdb
mejorf <mejorfelippe@gmail.com>
2017-03-12 10:07:38 -0300

    check for empty http.response.body

--- mejorsub.rb
+++ mejorsub.rb
@@ -9,6 +9,8 @@ class SubDownloader
   def initialize(options)
     @target = ARGV[0]
     @language = options[:language]
+    @count_sucess = 0
+    @count_fail = 0
     if File.directory?(@target)
       @ext = options[:extension] || @ext = '*/*.{avi,mp4,mkv,mpeg,flv,rm,wmv,
                                                  m4v}'
@@ -66,8 +68,14 @@ class SubDownloader
       download = http.send_request('GET', '/?action=download&hash=' + hash +
         "&language=#{@language}", nil, header)
       out = @files_and_hashes.key(hash).gsub(/\.[^.]*$/, '.srt')
-      File.open(out, 'wb') { |f| f.write(download.body) }
-      puts out + ' sucessfully downloaded'
+      unless download.body.empty?
+        File.open(out, 'wb') { |f| f.write(download.body) }
+        puts out + 'successfully downloaded'
+        @count_sucess += 1
+      else
+        puts out + 'failed'
+        @count_fail += 1
+      end
     rescue Exception => e
       puts e
     end
@@ -77,6 +85,9 @@ class SubDownloader
     @files_and_hashes.each do |_filename, hash|
       download hash
     end
+    puts "#{@count_sucess} file(s) successfully downloaded" unless
+                                                      @count_sucess < 1
+    puts "#{@count_fail} download(s) failed" unless @count_fail < 1
   end
 end
 
